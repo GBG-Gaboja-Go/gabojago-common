a42ff8cfbd94add417915d6d4939d3efc3844b30
changhui98 <net_0102@naver.com>
2025-11-04 22:48:08 +0900

    Swagger 오류 되는 부분 코드 변경

--- src/main/java/com/gabojago/swagger/config/SwaggerAutoConfig.java
+++ src/main/java/com/gabojago/swagger/config/SwaggerAutoConfig.java
@@ -3,9 +3,12 @@ package com.gabojago.swagger.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @AutoConfiguration
 @OpenAPIDefinition(
@@ -21,11 +24,23 @@ public class SwaggerAutoConfig {
     public OpenAPI commonOpenAPI() {
 
         return new OpenAPI()
-            .addServersItem(new Server().url("/"))
             .info(new io.swagger.v3.oas.models.info.Info()
                 .title("GabojaGo API 문서")
                 .version("v1.0")
                 .description("공통 Swagger 자동 구성"));
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
     }
 
 }
